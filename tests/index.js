7d9d667701f77be5ebf0b098f538b4e56e058505
Saswat Das <saswat.das@getpostman.com>
2020-03-11 21:45:41 +0530

    Add support for beforeAll and afterAll handlers

---
+++ tests/index.js
@@ -0,0 +1,35 @@
+const Caramel = require('../index'),
+  caramel = new Caramel(),
+  { Helper } = Caramel;
+
+
+class Tools extends Helper {
+  async beforeAll() {
+    return new Promise((r, j) => {
+      setTimeout(() => {
+        console.log('beforeAll');
+        r();
+      }, 2000);
+    });
+  }
+
+  async $pause() {
+    return new Promise((r, j) => {
+      setTimeout(() => {
+        console.log('pause');
+
+        r();
+      }, 2000);
+    });
+  }
+
+  async $something() {
+    return new Promise((r, j) => {
+      setTimeout(() => { r(2); }, 1000);
+    });
+  }
+}
+
+caramel.loadFiles('tests/workflows/**/*.flow.js');
+caramel.addHelper(new Tools());
+caramel.run();
---
+++ tests/workflows/send-request.flow.js
@@ -0,0 +1,6 @@
+feature('Feature 1');
+
+scenario('Scenario 1', (I) => {
+  I.pause();
+  I.pause();
+});
