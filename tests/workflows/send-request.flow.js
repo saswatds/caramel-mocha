cff979b5317e12900381cee158d58a668f47877c
Saswat Das <saswat.das@postmanlabs.com>
2020-03-14 01:56:55 +0530

    Add ability pause before each step

--- tests/workflows/send-request.flow.js
+++ tests/workflows/send-request.flow.js
@@ -3,4 +3,10 @@ feature('Feature 1').tag('abc');
 scenario('Scenario 1', (I) => {
   I.pause();
   I.pause();
-}).repeat(3);
+}).repeat(3).paused();
+
+
+scenario('Scenario 2', (I) => {
+  I.pause();
+  I.pause();
+}).repeat(3).paused();
