845043e717958e27abb142af1c5815fb8ff1df67
Saswat Das <saswat.das@postmanlabs.com>
2020-03-14 20:30:54 +0530

    Replace original printer with signal

--- tests/workflows/send-request.flow.js
+++ tests/workflows/send-request.flow.js
@@ -1,15 +1,14 @@
 feature('Feature 1').tag('abc');
 
 setup((I) => {
-  I.pause();
+  I.pause(1);
 });
 
 scenario('Scenario 1', (I) => {
-  I.pause();
-  I.pause();
+  I.pause(2);
+  I.pause(3, 4);
 });
 
 scenario('Scenario 2', (I) => {
-  I.pause();
   I.pause();
 });
